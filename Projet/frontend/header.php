148d5270c8a044d28b6c25268962dbeb174817a0
josh-bgd <josuaberdugo@gmail.com>
2023-03-29 12:02:54 +0200

    frontend

--- Projet/frontend/header.php
+++ Projet/frontend/header.php
@@ -1,13 +1,9 @@
 <!DOCTYPE html>
-<html lang="<?php echo $lang?>">
+<html>
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="style1.css">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="../../../assets/bootstrap/css/bootstrap.min.css">
-    <link href="../../../assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <script src="../../../assets/bootstrap/js/bootstrap.bundle.min.js"></script>
     <title><?php echo $currentPageId ?></title>
 </head>
 <body>
--- Projet/frontend/index.php
+++ Projet/frontend/index.php
@@ -8,12 +8,39 @@ require_once("header.php");
 ?>
 
 <header class="bandeau_haut">
-    <?php
-    require_once('login_form.php');
-    ?>
+    <form>
+        <label for="login">Login:</label>
+        <input type="text" id="login" name="login" required><br><br>
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" required><br><br>
+        <input type="submit" value="Submit" onclick="onloginSubmit()">
+    </form>
 </header>
 
-<section class="corps"></section>
+<section class="corps">
+
+</section>
+
+<script> // A METTRE DANS CRUD.JS
+    function onloginSubmit() {
+    let login = $("#login").val();
+    let password = $("#motdepasse").val();
+    if (nom.trim() !== '') {
+        $.ajax({
+            url: apifolder + '/login.php',
+            method: "POST",
+            data: JSON.stringify({ "login": login, "motdepasse": password }),
+            success: function (response) {
+                echo(response);
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                alert("Erreur pour vérifier les credentials");
+                console.log(textStatus, errorThrown);
+            }
+        });
+    }
+}
+</script>
 
 <?php
 require_once("footer.php");
