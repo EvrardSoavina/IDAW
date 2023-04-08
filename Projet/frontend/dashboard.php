7b73560b4253212dc319ccf60b1501173a4c1bb4
EvrardSoavina <evrsoavina@gmail.com>
2023-04-08 11:32:08 +0200

    ajout d'un footer.php

--- Projet/frontend/dashboard.php
+++ Projet/frontend/dashboard.php
@@ -280,55 +280,9 @@ require_once('cookie_session.php');
     </section>
 
     <footer class="site-footer-dashboard" role="contentinfo">
-
-        <div class="container">
-            <div class="row mb-5">
-                <div class="col-md-4 mb-5">
-                    <h3>About Us</h3>
-                    <p class="mb-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus et dolor blanditiis consequuntur ex voluptates perspiciatis omnis unde minima expedita.</p>
-                    <ul class="list-unstyled footer-link d-flex footer-social">
-                        <li><a href="#" class="p-2"><span class="fa fa-twitter"></span></a></li>
-                        <li><a href="#" class="p-2"><span class="fa fa-facebook"></span></a></li>
-                        <li><a href="#" class="p-2"><span class="fa fa-linkedin"></span></a></li>
-                        <li><a href="#" class="p-2"><span class="fa fa-instagram"></span></a></li>
-                    </ul>
-                </div>
-
-                <div class="col-md-4 b-5">
-                    <h3>Contact Info</h3>
-                    <ul class="list-unstyled footer-link">
-                        <li class="d-block">
-                            <span class="d-block text-black">Address:</span>
-                            <span>34 Street Name, City Name Here, United States</span>
-                        </li>
-                        <li class="d-block"><span class="d-block text-black">Phone:</span><span>+1 242 4942 290</span></li>
-                        <li class="d-block"><span class="d-block text-black">Email:</span><span>info@yourdomain.com</span></li>
-                    </ul>
-                </div>
-                <div class="col-md-4 mb-5">
-                    <h3>Quick Links</h3>
-                    <ul class="list-unstyled footer-link">
-                        <li><a href="#">Terms of Use</a></li>
-                        <li><a href="#">Contact</a></li>
-                    </ul>
-                </div>
-                <div class="col-md-3">
-
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-12 text-md-center text-left">
-                    <p>
-                        <!-- Link back to Free-Template.co can't be removed. Template is licensed under CC BY 3.0. -->
-                        <small class="block">&copy; 2019 <strong class="text-black">Foody</strong> Free Template. All Rights Reserved. <br> Design by <a href="https://free-template.co/" target="_blank">Free-Template.co</a></small>
-                    </p>
-                </div>
-            </div>
-        </div>
-    </footer>
-    <!-- END footer -->
-
+        
     <?php
+    require_once('footer.php');
     require_once('loader.php');
     ?>
 </body>
