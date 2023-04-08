8a2d2e5cc4799d2824b9391a709e37ce2c14c7ff
josh-bgd <josuaberdugo@gmail.com>
2023-04-08 16:55:22 +0200

    .

--- Projet/frontend/loader.php
+++ Projet/frontend/loader.php
@@ -22,7 +22,6 @@
   <script src="js/signup.js"></script>
   <script src="js/objectives.js"></script>
   <script src="js/aliments.js"></script>
-  <script src="js/testapi.js"></script>
 
   <script src="js/jquery.magnific-popup.min.js"></script>
   <script src="js/magnific-popup-options.js"></script>
